54a21e625e8ad6db48a269aa5c6109a98b5bdbe0
Kitsune Solar <kitsune.solar@gmail.com>
2018-07-13 23:44:21 +0300

    2018-07-13 20:44:21

--- MW_EXT_Tooltip.class.php
+++ MW_EXT_Tooltip.class.php
@@ -7,7 +7,7 @@ use MediaWiki\Extension\MW_EXT_Core\MW_EXT_Core;
 
 /**
  * Class MW_EXT_Tooltip
- * ------------------------------------------------------------------------------------------------------------------ */
+ */
 class MW_EXT_Tooltip {
 
 	/**
@@ -17,8 +17,7 @@ class MW_EXT_Tooltip {
 	 *
 	 * @return bool
 	 * @throws \MWException
-	 * -------------------------------------------------------------------------------------------------------------- */
-
+	 */
 	public static function onParserFirstCallInit( Parser $parser ) {
 		$parser->setFunctionHook( 'tooltip', [ __CLASS__, 'onRenderTag' ] );
 
@@ -33,8 +32,7 @@ class MW_EXT_Tooltip {
 	 * @param string $tooltip
 	 *
 	 * @return string
-	 * -------------------------------------------------------------------------------------------------------------- */
-
+	 */
 	public static function onRenderTag( Parser $parser, $word = '', $tooltip = '' ) {
 		// Argument: id.
 		$getWord = MW_EXT_Core::outClear( $word ?? '' ?: '' );
@@ -60,8 +58,7 @@ class MW_EXT_Tooltip {
 	 * @param Skin $skin
 	 *
 	 * @return bool
-	 * -------------------------------------------------------------------------------------------------------------- */
-
+	 */
 	public static function onBeforePageDisplay( OutputPage $out, Skin $skin ) {
 		$out->addModuleStyles( [ 'ext.mw.tooltip.styles' ] );
 
